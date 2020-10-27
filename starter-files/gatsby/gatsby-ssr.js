39b7acee4869dadb92d5067ea5915fceea38de31
“Lauren <lucerolaurenm@gmail.com>
2020-10-27 16:53:38 -0400

    Gatsby files

--- starter-files/gatsby/gatsby-ssr.js
+++ starter-files/gatsby/gatsby-ssr.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from './src/components/Layout';
 
-function wrapPageElement({element, props}) {
+export function wrapPageElement({element, props}) {
   return ( <Layout {...props}>{element}</Layout> );
 }
