207529695d26d28f4c4c9c96926054f9b5a25764
“Lauren <lucerolaurenm@gmail.com>
2020-10-29 16:43:11 -0400

    Create data relationships

--- starter-files/sanity/schemas/pizza.js
+++ starter-files/sanity/schemas/pizza.js
@@ -37,5 +37,32 @@ export default {
       validation: Rule => Rule.min(1000),
       // TODO: add custom input
     },
-  ]
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [{type: 'reference', to: [{type: 'topping'}]}]
+    }
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+    prepare: ({title, media, ...toppings}) => {
+      // filter undefined toppings out
+      const tops = Object.values(toppings).filter(Boolean)
+      // return preview object for the pizza
+      // 
+      return {
+        title,
+        media,
+        subtitle: tops.join(', ')
+      }
+    }
+  }
 }
